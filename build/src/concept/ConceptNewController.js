6ce15431d33f2c832189b55ace05aae14a25b2d6
Jeremy Wong <jermspeaks@gmail.com>
2015-04-24 15:48:14 -0500

    add and remove forms and concepts

--- build/src/concept/ConceptNewController.js
+++ build/src/concept/ConceptNewController.js
@@ -3,7 +3,14 @@
 module.exports = function($log, $scope) {
     // _______________
     // Scope Variables
-    $scope.form = {};
+    $scope.form = {
+        associatedForms: [{
+            id: 'Form 1'
+        }],
+        relatedConcepts: [{
+            id: 'Concept 1'
+        }]
+    };
 
     $scope.supportedEntities = [{
         name: 'Person'
@@ -17,11 +24,33 @@ module.exports = function($log, $scope) {
     // Scope Functions
 
     $scope.addConcept = function() {
-      $log.debug('Adding Form Field');
+        $log.debug('Adding Concept Field');
+
+        var newItemNo = $scope.form.relatedConcepts.length + 1;
+
+        $scope.form.relatedConcepts.push({'id':'Form ' + newItemNo});
     };
 
-    $scope.deleteConcept = function() {
+    $scope.deleteConcept = function(relatedConcept) {
+        $log.debug('Deleting Concept Field');
+        $scope.form.relatedConcepts = _.reject($scope.form.relatedConcepts, function(rConcept) {
+            return rConcept.$$hashKey === relatedConcept.$$hashKey;
+        });
+    };
 
+    $scope.addForm = function() {
+        $log.debug('Adding Form Field');
+
+        var newItemNo = $scope.form.associatedForms.length + 1;
+
+        $scope.form.associatedForms.push({'id':'Form ' + newItemNo});
+    };
+
+    $scope.deleteForm = function(associatedForm) {
+        $log.debug('Deleting Form Field');
+        $scope.form.associatedForms = _.reject($scope.form.associatedForms, function(aForm) {
+            return aForm.$$hashKey === associatedForm.$$hashKey;
+        });
     };
 
     $scope.submitNewConcept = function() {
