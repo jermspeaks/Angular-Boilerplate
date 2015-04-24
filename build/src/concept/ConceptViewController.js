a720f78e56326453174fbff91dec3fe0b58ed020
Jeremy Wong <jermspeaks@gmail.com>
2015-04-24 14:59:16 -0500

    no url change, need to figure out

--- build/src/concept/ConceptViewController.js
+++ build/src/concept/ConceptViewController.js
@@ -1,13 +1,21 @@
 'use strict';
 
-module.exports = function($log, $scope) {
+module.exports = function($log, $scope, $state, RootScopeService) {
     $log.log('ConceptViewController');
     // _______________
     // Scope Variables
     $scope.model = {};
 
     $scope.backToSearch = function() {
-      $log.log('Going back to search');
+        $log.debug('Back To Concept Search Results');
+        var search = RootScopeService.getSearch();
+
+        if (search) {
+            $state.transitionTo('concept.find.list', {q: search}); // TODO Can't figure out why the url won't change
+            // $state.go('concept.find.list', {q: search});
+        } else {
+            $state.go('concept.find');
+        }
     };
 
 };
--- dist/js/app.js
+++ dist/js/app.js
@@ -326,14 +326,22 @@ module.exports = function($log, $scope, $state, $stateParams) {
 },{}],10:[function(require,module,exports){
 'use strict';
 
-module.exports = function($log, $scope) {
+module.exports = function($log, $scope, $state, RootScopeService) {
     $log.log('ConceptViewController');
     // _______________
     // Scope Variables
     $scope.model = {};
 
     $scope.backToSearch = function() {
-      $log.log('Going back to search');
+        $log.debug('Back To Concept Search Results');
+        var search = RootScopeService.getSearch();
+
+        if (search) {
+            $state.transitionTo('concept.find.list', {q: search}); // TODO Can't figure out why the url won't change
+            // $state.go('concept.find.list', {q: search});
+        } else {
+            $state.go('concept.find');
+        }
     };
 
 };
